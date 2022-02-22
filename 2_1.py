f70896de3232adc90b48f326af29fd29aa7e77cc
Por Xiong <porxiong2020@outlook.com>
2022-02-22 14:23:07 -0800

    first commit

---
+++ 2_1.py
@@ -0,0 +1,61 @@
+## print(3 + 2, 3 -2 , 3 * 2)
+## print(8 / 2, 8 ** 2 , 2 * (3 + 4))
+## print("3/2=",3/2)
+# speed = 50 
+# time_elapsed = 14 
+# distance = speed * time_elapsed
+# print(distance)
+
+# change the string into lower case 
+# toLowerCase() 
+
+# a comment is 4 the programmer not python
+
+"""
+This is a multi line comment
+"""
+# this is a one line comment
+
+# a = 2 
+# b = 3
+# c = 4*b
+# d = 1-c 
+# e = abs(d)
+# print(e)
+# a = 2
+# b = 3
+# print(abs(1 - (4 * b)))
+# print(int((a ** b) + .8))
+# print(round(a / b, 3))
+
+# a = 2
+# b = 3
+# print (abs(1-4*b))
+
+# num = 4 
+# num2 = num
+# num3 = num2
+# num3 = 0
+# num3 += num2 # not valid 
+# num3 -= 2
+# print("num3", num3)
+# num3 =+ num3 
+# print("num3 += num3", num3 =+ 4) 
+# x = 0
+# x = x + 1 # x +=1
+# x = x - 1 # x -=1
+
+total_inches = 41
+feet = total_inches // 12
+inches = total_inches % 12
+print(feet, inches)
+
+inches = 10 % 2 
+print("inches", inches)
+
+inches = 10 % 3 
+print("inches", inches)
+
+inches = 10 // 3 # int method return integer
+print("inches", inches)
+
