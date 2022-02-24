865355f1d35970be636ec70dd319fbf302183689
Por Xiong <porxiong2020@outlook.com>
2022-02-24 15:27:09 -0800

    first commit

--- 2_3.py
+++ 2_3.py
@@ -1,1 +1,16 @@
-print("chapter 2-3")
+# print("chapter 2-3")
+print("one", "two", "three", sep=",")
+
+print("Hello")
+# print("Hello", end=" ",) # \r
+print("World!")
+
+#print("a\tb\tc") #a     b   c
+#print("a\tb\tc\nd\te\tf") #a    b   c
+                          #d    e   f
+
+print('"Hello World!"') # "Hello World!"
+# print(""Hello World!"") # "Hello World!"
+print("\"Hello World!\"") # "Hello World!"
+#print("\Hello World!\")  "Hello World!"
+print("\\Hello World!\\") # "Hello World!"
