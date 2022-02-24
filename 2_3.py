04552ff645728989f02c93776869fd3593396ee6
Por Xiong <porxiong2020@outlook.com>
2022-02-24 15:40:40 -0800

    first commit

--- 2_3.py
+++ 2_3.py
@@ -1,16 +1,22 @@
-# print("chapter 2-3")
-print("one", "two", "three", sep=",")
+# # print("chapter 2-3")
+# print("one", "two", "three", sep=",")
 
-print("Hello")
-# print("Hello", end=" ",) # \r
-print("World!")
+# print("Hello")
+# # print("Hello", end=" ",) # \r
+# print("World!")
 
-#print("a\tb\tc") #a     b   c
-#print("a\tb\tc\nd\te\tf") #a    b   c
-                          #d    e   f
+# #print("a\tb\tc") #a     b   c
+# #print("a\tb\tc\nd\te\tf") #a    b   c
+#                           #d    e   f
 
-print('"Hello World!"') # "Hello World!"
-# print(""Hello World!"") # "Hello World!"
-print("\"Hello World!\"") # "Hello World!"
-#print("\Hello World!\")  "Hello World!"
-print("\\Hello World!\\") # "Hello World!"
+# print('"Hello World!"') # "Hello World!"
+# # print(""Hello World!"") # "Hello World!"
+# print("\"Hello World!\"") # "Hello World!"
+# #print("\Hello World!\")  "Hello World!"
+# print("\\Hello World!\\") # "Hello World!"
+
+print("0123456789012345678901234567")
+print("Rank".ljust(5), "Player".ljust(20), "HR".rjust(3), sep="")
+print('1'.center(5), "Barry Bonds".ljust(20), "762".rjust(3), sep= "")
+print('2'.center(5), "Hank Aaron".ljust(20), "755".rjust(3), sep= "")
+print('3'.center(5), "Babe Ruth".ljust(20), "714".rjust(3), sep= "")
