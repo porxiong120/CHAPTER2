3255004cf31b05fad05a37ae0f66fe1b4cab1dc5
Por Xiong <porxiong2020@outlook.com>
2022-02-24 14:47:36 -0800

    first commit

---
+++ 2_2.py
@@ -0,0 +1,91 @@
+
+#myvar = "Say it ain't so" 
+# myvar = 'Say it ain' + 't so'
+#print(myvar)
+#var2 = "Spam & Eggs"
+# print("var2", var2)
+# var2[0]
+# print(var2[0])
+# print(var2[-1])
+# var2[0:3]
+# print(var2[0:3])
+# print(var2[0:4])
+# print(var2[0:])
+## print(var2[:4])
+#spam = "Spam"
+#eggs = "Eggs"
+#spam_and_eggs = spam + " & " + eggs
+#print(spam_and_eggs [:])
+
+#length = len(spam_and_eggs)
+#print(length)
+#print(spam_and_eggs.upper())
+#print(spam_and_eggs.lower())
+
+#var2 = "Spam & Eggs"
+#spam = "Spam      "
+#eggs = "     Eggs"
+
+# praise = "Good Doggie" #.upper()
+# uppercase = "GOOD DOGGIE"
+# #count = praise.count("G")
+# #count = praise.upper().count("G")
+# count = praise.uppercase().count("G")
+# # praise.upper() = "GOOD DOGGIE"
+# print("Count: ", count)
+
+# town = input("ENter the town of your city: ")
+
+
+# full_name = input("Enter a full name: ")
+# n = full_name.rfind(" ") # return int of where the space is
+# # index_of_space = full_name.rfind(" ")
+# # first_name = full_name[0:index_of_space]
+# print("Last name:", full_name[n+1:])
+# print("First name(s):", full_name[:n])
+
+# full_name = input("Enter your full name: ")
+# index_of_space = full_name.rfind(" ")
+# first_name = full_name[:index_of_space]
+# last_name = full_name[index_of_space+1:]
+# print("First_name", first_name)
+# print("Last name", last_name)
+
+# annual_income = input("Enter your annual income: ")
+# print("User's income is ", annual_income)
+# monthly_income = annual_income/12
+# print("monthly income", monthly_income)
+
+# annual_income = eval(input("Enter your annual income: "))
+# print("User's income is ", annual_income)
+# monthly_income = annual_income/12
+# print("monthly income", monthly_income)
+
+# annual_income = input("Enter your annual income: ")
+# print("User's income is ", annual_income)
+# monthly_income = eval(annual_income)/12
+# print("monthly income", monthly_income)
+
+# annual_income = int(input("Enter your annual income: "))
+# print("User's income is ", annual_income)
+# monthly_income = annual_income/12
+# print("monthly income", monthly_income)
+
+# var1 = 1
+# var2 = 2
+# var3 = "3"
+# var4 = "4"
+# var5 = var1+var2 # equal 3 int type
+# var6 = var3+var4 # equal 34 string type
+
+
+str1 = "Python"
+length = len(str1) # = 5 int tyep - length of string 
+str2 = str1[:-1] # = Python string type 
+str2 = str1[:] # = 
+print("str2", str2)
+# str3 = str1[10] # no 10th index 
+str3 = str1[:10]
+print("str3", str3)
+str4 = str1[2:-1] # = tho
+print("str4", str4)
